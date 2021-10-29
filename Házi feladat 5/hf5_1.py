8849d895c7659b299edd226fffb3f0da0c7cfd5f
Arameister04 <arameister04@gmail.com>
2021-10-29 15:27:22 +0200

    7. Óra

--- Házi feladat 5/hf5_1.py
+++ Házi feladat 5/hf5_1.py
@@ -1,7 +1,12 @@
 filepath = "lorem_copy.txt"
 fileobject = open(filepath, "r")
 lines = fileobject.readlines()
-characters = fileobject.read()
 print("A sorok száma: ", len(lines))
-print("A karakterek száma: ", len(characters))
+fileobject.close()
+
+
+fileobject = open(filepath, "r")
+characters = fileobject.read().replace(" ", "")
+len_of_char = len(characters)
+print("A karakterek száma: ", len_of_char)
 fileobject.close()
