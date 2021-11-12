b64a7ba7e59037ad93007a067fdb46888e3a8a79
Arameister04 <arameister04@gmail.com>
2021-11-12 16:23:32 +0100

    9. Óra

---
+++ 9. ora/lista min,max,atlag.py
@@ -0,0 +1,13 @@
+#ÁTLAG
+sum = 0
+for i  in range(len(lista))
+    sum += lista[i]
+atlag = sum / len(lista)
+
+
+#MIN VAGY MAX ÁTÍRVA
+min = lista[0]
+for i in range(len(lista))
+    if min > lista[i]
+        min = lista[i]
+
